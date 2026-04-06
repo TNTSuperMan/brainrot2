30db2f94434f429f7975adbb42a3e3307db483e8
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-04-06 11:52:19 +0900

    dot出力でつながられるとこを繋げる

--- src/cfg/dot.rs
+++ src/cfg/dot.rs
@@ -5,16 +5,14 @@ pub fn cfg_to_dot(cfg: &CFG) -> String {
     dot.push_str("digraph {\n");
 
     for (i, node) in cfg.0.iter().enumerate() {
-        dot.push_str(&format!("    n{i} [\n"));
-        dot.push_str(&format!("        label=\"n{i}\\l"));
+        dot.push_str(&format!("    n{i} [\n        label=\"n{i}\\l"));
         for inst in &node.insts {
             dot.push_str(&format!("{inst:?}\\l"));
         }
         if let Some(offset) = node.offset {
             dot.push_str(&format!("offset {offset}\\l"));
         }
-        dot.push_str(&format!("{:?}\\l", node.edge));
-        dot.push_str("\"\n");
+        dot.push_str(&format!("{:?}\\l\"\n", node.edge));
         if node.offset.is_some() {
             dot.push_str(&format!("        shape=octagon\n"));
         } else if node.insts.len() != 0 || node.offset.is_some() {
