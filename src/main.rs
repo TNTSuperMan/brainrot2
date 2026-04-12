16e7a82954d26f94fdec6abafbf644344cd69a70
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-04-12 11:52:25 +0900

    もっとちゃんとした開発用CLIを作る

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{env::args, fs, process::ExitCode};
 
 use crate::{
     cfg::{cfg::CFG, dot::cfg_to_dot, int::exec_from_cfg},
@@ -10,13 +10,33 @@ mod error;
 mod ir;
 mod ssa;
 
-fn main() {
-    let code = fs::read_to_string("box/mandel.bf").unwrap();
-    let ir = IR::parse(&code).unwrap();
-    //exec_from_ir(&ir);
-    let cfg = CFG::new(&ir);
-    //println!("{}", cfg_to_dot(&cfg));
-    //println!("{ir:?}");
-    println!("{cfg:?}");
-    //exec_from_cfg(&cfg);
+fn main() -> ExitCode {
+    let args = args();
+    if let [_, _, kind, file] = args.collect::<Vec<String>>().as_slice() {
+        let code = fs::read_to_string(&file).unwrap();
+        let ir = IR::parse(&code).unwrap();
+        let cfg = CFG::new(&ir);
+        match kind.as_str() {
+            "exec_ir" => {
+                exec_from_ir(&ir);
+            }
+            "exec_cfg" => {
+                exec_from_cfg(&cfg);
+            }
+            "dump_ir" => {
+                println!("{ir:?}");
+            }
+            "dump_cfg" => {
+                println!("{cfg:?}");
+            }
+            "print_cfg_dot" => {
+                println!("{}", cfg_to_dot(&cfg));
+            }
+            _ => {
+                eprintln!("unknown kind");
+                return ExitCode::FAILURE;
+            }
+        }
+    }
+    return ExitCode::FAILURE;
 }
