8cd004e40c8c9cd8b0fb8b43df833f1c69d79300
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-04-13 14:13:31 +0900

    CFG to SSA用の一部関数を

---
+++ src/ssa/parse.rs
@@ -0,0 +1,58 @@
+use crate::{
+    cfg::cfg::{CFG, CFGOpKind},
+    ssa::ssa::SSAProgram,
+};
+
+struct LatestVersionsMap(Vec<usize>);
+impl LatestVersionsMap {
+    fn pointer_to_map_address(&self, pointer: isize) -> usize {
+        (if pointer < 0 {
+            (-pointer) * 2 + 1
+        } else {
+            pointer * 2
+        }) as usize
+    }
+    fn alloc(&mut self, pointer: isize) -> usize {
+        let addr = self.pointer_to_map_address(pointer);
+        if self.0.len() >= addr {
+            self.0.resize(addr + 1, 0);
+        }
+        let ver = self.0[addr];
+        self.0[addr] = ver + 1;
+        ver
+    }
+}
+
+fn compute_version_map(cfg: &CFG) -> Vec<Vec<usize>> {
+    let mut latest_version_map = LatestVersionsMap(vec![]);
+
+    cfg.0
+        .iter()
+        .map(|block| {
+            block
+                .insts
+                .iter()
+                .map(|inst| match inst.opcode {
+                    CFGOpKind::Breakpoint | CFGOpKind::Out => 0,
+                    CFGOpKind::Add(_)
+                    | CFGOpKind::Set(_)
+                    | CFGOpKind::MulAdd(..)
+                    | CFGOpKind::In => latest_version_map.alloc(inst.pointer),
+                })
+                .collect()
+        })
+        .collect()
+}
+
+impl SSAProgram {
+    pub fn new(cfg: &CFG) -> SSAProgram {
+        let mut blocks = vec![];
+        let version_map = compute_version_map(cfg);
+
+        for block in &cfg.0 {
+            // todo
+        }
+
+        SSAProgram(blocks)
+    }
+}
