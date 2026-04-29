95b404b439435f4f0345e437dfe15a387e854682
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-04-30 02:02:51 +0900

    CLIで最適化をデフォにする

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,12 @@ fn main() -> ExitCode {
         let code = fs::read_to_string(&file).unwrap();
         let ir = IR::parse(&code).unwrap();
         let mut cfg = CFG::new(&ir);
+        for _ in 0..3 {
+            cfg.inline_branch();
+            cfg.inline_flow();
+            cfg.fold_jump();
+            cfg.eliminate_dead_code();
+        }
         match kind.as_str() {
             "exec_ir" => {
                 exec_from_ir(&ir);
@@ -27,24 +33,6 @@ fn main() -> ExitCode {
                 println!("{cfg:?}");
             }
             "print_cfg_dot" => {
-                println!("{}", cfg_to_dot(&cfg));
-            }
-            "exec_opt_cfg" => {
-                for _ in 0..3 {
-                    cfg.inline_branch();
-                    cfg.inline_flow();
-                    cfg.fold_jump();
-                    cfg.eliminate_dead_code();
-                }
-                exec_from_cfg(&cfg);
-            }
-            "print_opt_cfg_dot" => {
-                for _ in 0..3 {
-                    cfg.inline_branch();
-                    cfg.inline_flow();
-                    cfg.fold_jump();
-                    cfg.eliminate_dead_code();
-                }
                 println!("{}", cfg_to_dot(&cfg));
             }
             _ => {
