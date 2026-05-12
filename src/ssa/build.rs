454ba728f8c1de3f17f4552213ac75effe99c940
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-05-12 14:59:18 +0900

    build_ssa作ってく1

--- src/ssa/build.rs
+++ src/ssa/build.rs
@@ -1,12 +1,46 @@
-use crate::{cfg::cfg::CFG, ssa::defines::program::SSAProgram};
+use crate::{cfg::cfg::{CFG, CFGEdge, CFGExpr, CFGOp, CFGValue}, ssa::defines::{block::{SSABlock, SSAEdge}, op::{SSAExpr, SSAOp}, program::SSAProgram, value::{SSAValue, SSAVersion}}};
+
+fn cv2sv(cv: &CFGValue) -> SSAValue {
+    match cv {
+        CFGValue::Const(val) => SSAValue::Const(*val),
+        CFGValue::Load(ptr) => SSAValue::Version(SSAVersion { pointer: *ptr, version: u32::MAX }),
+    }
+}
 
 pub fn build_ssa(cfg: &CFG) -> SSAProgram {
-    let mut blocks = vec![];
-    let mut block_offset = 0;
+    let mut ver = 0;
 
-    let blocks = cfg.0.iter().map(|block| {
-        
-    })
+    let mut blocks = cfg.0.iter().map(|block| {
+        SSABlock {
+            alive: block.alive,
+            predecessor: block.predecessor.clone(),
+            edge: match block.edge {
+                CFGEdge::Jump(to) => SSAEdge::Jump(to),
+                CFGEdge::Branch { pointer, zero, nonzero } => SSAEdge::Branch { version: SSAVersion { pointer, version: u32::MAX }, zero, nonzero, ir_at: None },
+                CFGEdge::BranchWithIRAt { pointer, zero, nonzero, ir_at } => SSAEdge::Branch { version: SSAVersion { pointer, version: u32::MAX }, zero, nonzero, ir_at: Some(ir_at) },
+                CFGEdge::End => SSAEdge::End,
+            },
+            insts: block.insts.iter().map(|inst| {
+                match inst {
+                    CFGOp::Out(val) => SSAOp::Out(cv2sv(val)),
+                    CFGOp::Assign(ptr, expr) => {
+                        let v = SSAVersion { pointer: *ptr, version: ver };
+                        ver += 1;
+
+                        match expr {
+                            CFGExpr::Value(val) => SSAOp::Hint(v, cv2sv(val)),
+                            CFGExpr::Add(v1, v2) => SSAOp::Assign(v, SSAExpr::Add(cv2sv(v1), cv2sv(v2))),
+                            CFGExpr::Sub(v1, v2) => SSAOp::Assign(v, SSAExpr::Sub(cv2sv(v1), cv2sv(v2))),
+                            CFGExpr::Mul(v1, v2) => SSAOp::Assign(v, SSAExpr::Mul(cv2sv(v1), cv2sv(v2))),
+                            CFGExpr::MulAdd(v1, v2, v3) => SSAOp::Assign(v, SSAExpr::MulAdd(cv2sv(v1), cv2sv(v2), *v3)),
+                            CFGExpr::In => SSAOp::In(v),
+                        }
+                    }
+                }
+            }).collect(),
+            offset: block.offset,
+        }
+    }).collect();
 
     
 
